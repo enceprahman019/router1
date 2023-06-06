0e6050583c3393c668862d01b450b5a6db8713e7
Encep Rahman Armana <128059459+enceprahman019@users.noreply.github.com>
2023-06-06 18:51:39 +0700

    Add files via upload

---
+++ bookscomponent.js
@@ -0,0 +1,47 @@
+export const BooksComponents = {
+  data() {
+    return {
+      books: [
+        {
+          id: 12,
+          title: 'Belajar Matematika',
+          description: 'Belajar rumus-rumus secara cepat',
+          authors: 'jhon Doe',
+          publish_year: 2019,
+          price: 100000,
+          Image: '',
+        },
+        {
+          id: 13,
+          title: 'Belajar Bahasa Inggris',
+          description: 'Belajar Berbahasa inggris ',
+          authors: 'jhon Doe',
+          publish_year: 2019,
+          price: 500000,
+          Image: '',
+        },
+        {
+          id: 12,
+          title: 'Belajar Bahasa indonesia',
+          description: 'Belajar rumus-rumus secara cepat',
+          authors: 'jhon Doe',
+          publish_year: 2019,
+          price: 100000,
+          Image: '',
+        },
+      ],
+    };
+  },
+  template: `
+        <div>
+            <h1>Daftar Buku </h1>
+       <ul>
+        <li v-for="book of books">
+        <router-link :to="'/book/'+book.id">
+        {{ book.tittle }}
+        </router-link>
+        </li>
+       </ul>
+        </div>
+            `,
+};
